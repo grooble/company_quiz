e6b77689da6fd34e0143b3d2bcf803cfa60de55f
Paul Bartels <grooble@gmail.com>
2013-03-18 16:37:44 +0900

    check-answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,13 +2,19 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
-	@answersarray = [@question.correct, @question.option1, @question.option2, @question.option3].shuffle
+	#@option_value = question.try(params[:option])
   end
   
   def ask
+  
   end
   
   def answer
-    
+    @question = Question.find(params[:id])
+    @option_value = @question.try(params[:option])
+	@right_answer = false
+	if params[:option] == "correct"
+	  @right_answer = true
+	end
   end
 end
