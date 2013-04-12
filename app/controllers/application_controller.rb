35f89acaf5aa77a47e104f6dc9162b0358643913
Paul Bartels <grooble@gmail.com>
2013-04-12 20:10:15 +0900

    mobile0412

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   #respond_to_mobile_requests :skip_xhr_requests => false
   include Mobylette::RespondToMobileRequests
   include SessionsHelper
+  
+  private
+
+    def is_mobile_request?
+      true
+    end
+
 end
