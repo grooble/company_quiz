4e0a9cc477b96e23494d8098b8fd8a2d3fd27fb4
Paul Bartels <grooble@gmail.com>
2013-09-15 08:58:46 +0900

    working mobile 130915

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  #include Mobylette::RespondToMobileRequests
+  include Mobylette::RespondToMobileRequests
   include SessionsHelper
     
+  
+    mobylette_config do |config|
+      config[:skip_xhr_requests] = false
+    end
+	
   #private
     #def is_mobile_request?
     #  true
