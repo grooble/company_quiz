9614e810a8dee75ba2b09f6f23eb1d2a3f047b1d
Paul Bartels <grooble@gmail.com>
2013-09-18 07:24:28 +0900

    cache fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,22 @@ class ApplicationController < ActionController::Base
   include Mobylette::RespondToMobileRequests
   include SessionsHelper
     
+  before_filter :set_cache_buster
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
   
-    mobylette_config do |config|
-      config[:skip_xhr_requests] = false
-    end
+  mobylette_config do |config|
+    config[:skip_xhr_requests] = false
+  end
 	
   #private
     #def is_mobile_request?
     #  true
-    #end
+  #end
 
 
 end
